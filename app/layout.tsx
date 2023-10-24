465aaf469df4293bbb66efff96686e511f1ef3be
Mahdi Tasha <imwhdiiii@gmail.com>
2023-10-24 16:29:19 +0330

    added layout page

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+// Code by mahdi tasha
+// Importing part
+import {ReactNode} from 'react';
+import {Metadata} from "next";
+
+// Defining metadata of pages
+const metadata:Metadata = {
+    title: 'Coffee Catch Up - Daily blog app',
+    description: 'Coffee Catch Up is daily my (Mahdi Tasha) daily weblog :))',
+    viewport: 'width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0',
+    themeColor: '#00000',
+    openGraph: {
+        title: 'Coffee Catch Up - Daily blog app',
+        description: 'Coffee Catch Up is daily my (Mahdi Tasha) daily weblog :))',
+        type: "website"
+    },
+    twitter:{
+        title: 'Coffee Catch Up - Daily blog app',
+        description: 'Coffee Catch Up is daily my (Mahdi Tasha) daily weblog :))',
+    }
+};
+
+// Defining type of props
+interface propsType {
+    children: ReactNode;
+}
+
+// Creating and exporting layout component as default
+export default function Layout({children}:propsType):ReactNode {
+    // Returning JSX
+    return (
+        <html>
+            <body>
+                {children}
+            </body>
+        </html>
+    );
+}
