f8dad690dae43a007b287c34fe35fa9c6714e793
Mahdi Tasha <imwhdiiii@gmail.com>
2023-10-24 16:33:03 +0330

    added folder structures and added some configs of tailwindCSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,20 @@
+// Importing tailwindCSS configs
 import type { Config } from 'tailwindcss'
 
+// Defining configs of tailwindCSS
 const config: Config = {
-  content: [],
+  content: [
+      './app/*.{tsx,jsx,js,ts}',
+      './app/**/*.{tsx,jsx,js,ts}',
+      './app/**/**/*.{tsx,jsx,js,ts}',
+      './component/*.{tsx,jsx,js,ts}',
+      './component/**/*.{tsx,jsx,js,ts}',
+      './chunk/*.{tsx,jsx,js,ts}',
+      './chunk/**/*.{tsx,jsx,js,ts}',
+  ],
   theme: {},
   plugins: [],
 }
-export default config
+
+// Exporting the configs
+export default config;
